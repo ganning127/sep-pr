86f33d5f7fb2c7c606ab355b2d3f29e647addaf9
Ganning Xu <ganningxu@gmail.com>
2021-07-24 17:24:10 -0400

    Update test.1.2.js

--- .bit/tests/test.1.2.js
+++ .bit/tests/test.1.2.js
@@ -1,40 +1,31 @@
 const args = require('minimist')(process.argv.slice(2))
 const fetch = require('node-fetch');
+
 const user = args['user'];
 const repo = args['repo'];
-const endpoint = "https://counselorbot.azurewebsites.net/api/hasuraErrorUpdate?code=qL2oUjo1aUIBdfJe3VhEF41qRQBSnShZwPGr3dujRwvtOGa855fLoA==";
 
-let hello = undefined
+
+let hello;
 
 async function main() {
     try { hello = require('./../../week1/helloworld.js') }
-    catch(e) {
-
+    catch (e) {
         console.log("going to throw error");
-
-        await throwError("Searching for \"helloworld.js\"... file cannot be found")
-                .catch(e) {
-                    throw new Error("Error! Something bad happened");    
-                }
+        await throwError("Searching for 'helloworld.js'... file cannot be found");
     }
 
     let helloworld = hello()
     let test_output = "Hello World"
 
-    if (helloworld != test_output){
-
-        await throwError(`Got: "${helloworld}", was expecting: "${test_output}".`)
-            .catch(e) {
-                    throw new Error("Error! Something bad happened");    
-              }
+    if (helloworld != test_output) {
+        await throwError(`Got: '${helloworld}', was expecting: '${test_output}'.`)
     }
 
-console.log("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
+    console.log("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
 }
 
-main();
-
 async function throwError(error) {
+    const endpoint = "https://counselorbot.azurewebsites.net/api/hasuraErrorUpdate?code=qL2oUjo1aUIBdfJe3VhEF41qRQBSnShZwPGr3dujRwvtOGa855fLoA==";
     options = {
         method: "POST",
         headers: {
@@ -43,7 +34,11 @@ async function throwError(error) {
             error
         }
     }
-    await fetch(endpoint, options);
+    await fetch(endpoint, options)
+
     throw new Error(error);
+
     console.log("fetch has been made");
 }
+
+main();
