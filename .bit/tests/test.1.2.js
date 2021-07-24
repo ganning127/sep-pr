6df742631d97791b93298493d7eeaafa6beb2e22
Ganning Xu <ganningxu@gmail.com>
2021-07-24 09:06:04 -0400

    Update test.1.2.js

--- .bit/tests/test.1.2.js
+++ .bit/tests/test.1.2.js
@@ -1,15 +1,47 @@
+const args = require('minimist')(process.argv.slice(2))
+const fetch = require('node-fetch');
+const user = args['user'];
+const repo = args['repo'];
+const endpoint = "https://counselorbot.azurewebsites.net/api/hasuraErrorUpdate?code=qL2oUjo1aUIBdfJe3VhEF41qRQBSnShZwPGr3dujRwvtOGa855fLoA==";
+
 let hello = undefined
 
 try { hello = require('./../../week1/helloworld.js') }
 catch(e) {
-    throw new Error("Searching for \"helloworld.js\"... file cannot be found");
+    let error = "Searching for \"helloworld.js\"... file cannot be found"
+    
+    options = {
+        method: "POST",
+        headers: {
+            user,
+            repo,
+            error
+        }
+    }
+    await fetch(endpoint, options)
+    console.log("fetch has been made");
+    
+    throw new Error(error);
 }
 
 let helloworld = hello()
 let test_output = "Hello World"
 
-if(helloworld != test_output){
-    throw new Error(`Got: "${helloworld}", was expecting: "${test_output}".`)
+if (helloworld != test_output){
+    let error2 = `Got: "${helloworld}", was expecting: "${test_output}".`
+    
+    options = {
+        method: "POST",
+        headers: {
+            user,
+            repo,
+            error: error2
+        }
+    }
+    await fetch(endpoint, options)
+    console.log("fetch has been made")
+    
+    throw new Error(error2)
 }
 
 console.log("Yay! 🎉🎉🎉🎉🎉🎉🎉🍾")
