f54ce048c5d02e0cbb17a091a7bf5d6200ebef2c
Ganning Xu <ganningxu@gmail.com>
2021-07-24 20:14:05 -0400

    Update test.1.4.js

--- .bit/tests/test.1.4.js
+++ .bit/tests/test.1.4.js
@@ -1,51 +1,77 @@
-let uri = undefined
+let uri;
+const args = require('minimist')(process.argv.slice(2))
 const fetch = require('node-fetch');
-
 const fs = require('fs') //get the methods in the fs package
 
-let endpoint = undefined
+const user = args['user'];
+const repo = args['repo'];
+
+let endpoint;
 
 uri = process.env.HACKERVOICE_ENDPOINT;
 
-if (uri[0] != "h") {
-   throw new Error("You have not added your function url as a secret!");
-}
+async function main() {
+    if (uri == undefined || uri[0] != "h") {
+        // await throwError("You have not added your function url as a secret!");
+    }
 
-//if you wanna add more files, just put a comma after the filename (array)
+    //if you wanna add more files, just put a comma after the filename (array)
 
-const commit_file = ['hackervoice/index.js']
+    const commit_file = ['hackervoice/index.js']
 
-for(var i = 0; i < commit_file.length; i++) {
-    var a = commit_file[i];
-    fs.access(commit_file[i], err => {
-        if (err) {
-            throw new Error("You did not commit '" + a + "'")
+    for (var i = 0; i < commit_file.length; i++) {
+        var a = commit_file[i];
+        fs.access(commit_file[i], (err) => {
+            if (err) {
+                throwError(`You did not commit '${a}'`);
+            }
+        })
+    }
+
+    test1 = "fifiiscool";
+    uri1 = uri + "&password=" + test1;
+    if (uri[0] != "h") {
+        await throwError("You have not added your function url as a secret!");
+    }
+    try {
+        try {
+            const resp = await fetch(uri1, {
+                method: 'GET'
+            });
+            var data = await resp.text()
+            let test = JSON.stringify(data)
+
+            if (data == test1) {
+                console.log("Yay! 🎉 We got: " + JSON.stringify(data) + ", which matches our input.")
+            } else {
+                await throwError(`We got this '${JSON.stringify(data)}'. We should have gotten our input, 'fifiiscool' ... Try again!`);
+            }
         }
-    })
+        catch (e) { await throwError(`Try again! We got this error when trying to make a request: ${e}`); }
+    } catch (e) {
+        throw new Error("You have not added your function url as a secret!");
+    }
+
 }
 
+main();
 
-test1 = "fifiiscool";
-uri1 = uri + "&password=" + test1;
-if (uri[0] != "h") {
-   throw new Error("You have not added your function url as a secret!");
-}
 
-try {
-    (async () => {
-        const resp = await fetch(uri1, {
-            method: 'GET'
-        });
-        var data = await resp.text()
-        let test = JSON.stringify(data)
 
-        if (data == test1) {     
-            console.log("Yay! 🎉 We got: " + JSON.stringify(data) + ", which matches our input.")
-        } else {
-            console.log("We got this " + JSON.stringify(data) + ". We should have gotten our input, 'fifiiscool' ... Try again!")
-            process.exit(1)
+
+
+async function throwError(error) {
+    const endpoint = "https://counselorbot.azurewebsites.net/api/hasuraErrorUpdate?code=qL2oUjo1aUIBdfJe3VhEF41qRQBSnShZwPGr3dujRwvtOGa855fLoA==";
+    options = {
+        method: "POST",
+        headers: {
+            user,
+            repo,
+            error
         }
-    })().catch( e => { console.error("Try again! We got this error when trying to make a request: " + e); process.exit(1) })
-} catch (e) {
-    throw new Error("You have not added your function url as a secret!");
+    }
+    await fetch(endpoint, options)
+
+    process.exit(1);
 }
+
